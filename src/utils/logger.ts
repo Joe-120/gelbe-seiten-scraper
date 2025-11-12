06e691e19ee3d1e2954575268d9a2e59eab4d1cb
Joe-120 <kristenandrade8352@cutduk.com>
2025-11-12 15:17:48 -0600

    feat: add src/utils/logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,12 @@
+import pino from "pino";
+
+export function createLogger(level: string = "info") {
+return pino({
+level,
+transport: {
+target: "pino-pretty",
+options: { colorize: true, translateTime: "SYS:standard" }
+},
+base: undefined
+});
+}
